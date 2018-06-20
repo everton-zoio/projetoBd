9a4660101e5f437672189f7a705e2b8643f05dbd
LDeolindo <lucsan.1999@alunos.utfpr.edu.br>
2018-06-20 20:14:10 -0300

    commit

--- projeto/app.js
+++ projeto/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-global.db = require("./database/database");
+//global.db = require("./database/database");
 var app = express();
 
 // view engine setup
--- projeto/routes/index.js
+++ projeto/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/tables', function(req, res, next) {
+  res.render('tables', { title: 'Express' });
+});
+
+router.get('/blank', function(req, res, next) {
+  res.render('blank', { title: 'Express' });
+});
+
+router.get('/duvida', function(req, res, next) {
+  res.render('duvida', { title: 'Express' });
+});
+
 module.exports = router;
