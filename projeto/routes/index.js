a18c654b31bd5d438faec88c5d7149683ed9e0de
everton-zoio <evertonabreu@alunos.utfpr.edu.br>
2018-06-20 22:46:13 -0300

    Adicionado Crud de Usuario

--- projeto/routes/index.js
+++ projeto/routes/index.js
@@ -3,15 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(!logged)
+    res.redirect("/cadastro");
+  else
+    res.render("/", {});
 });
-
+router.get("/index", function(req, res, next){
+  res.redirect("../");
+});
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
+
+router.post("/login", function(req, res){
+  res.redirect("/");
+});
+
 router.get('/cadastro', function(req, res, next) {
   res.render('cadastro', { title: 'Express' });
 });
+router.post("/cadastro", function(req, res, next){
+  var body = req.body;
+  var sql = "INSERT INTO Usuario VALUES ('"+ body.ra + "', '" + body.nome + "', '" + body.password + "')";
+  db.query(sql, function(err, result, field){
+    if(err){
+      console.log(err);
+    }
+      logged = true;
+      res.render("index", {});
+
+  });
+});
+
 router.get('/tables', function(req, res, next) {
   res.render('tables', { title: 'Express' });
 });
